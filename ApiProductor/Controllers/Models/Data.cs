78631ffdb55b12e822c435b656ab670fe39013c1
aldo larrain <aldo.larrain@hotmail.com>
2021-03-13 08:45:27 -0400

    minuto 27

---
+++ ApiProductor/Controllers/Models/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProductor.Controllers.Models
+{
+    public class Data
+    {
+        [Key]
+        public string NameDevice { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime EventDate { get; set; }
+        
+        [Required]
+        public string Event { get; set; }
+
+    }
+}
